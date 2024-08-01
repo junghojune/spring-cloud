8fd93cbbd718a5dbd6b4710971c3ceb878cce881
hosu <hojune0904@gmail.com>
2024-08-01 14:55:28 +0900

    eureka 관련 학습

---
+++ com.spring-cloud.eureka-client.second/src/main/java/com/spring_cloud/eureka_client/second/SecondApplication.java
@@ -0,0 +1,13 @@
+package com.spring_cloud.eureka_client.second;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondApplication.class, args);
+	}
+
+}
